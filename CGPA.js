e3c0e26fa8a9f7dcac95d76849538373b51fd439
toufik0661 <toufiqurrahman0661@gmail.com>
2025-10-07 23:06:52 +0600

    CGPA Calculator in if-else

---
+++ CGPA.js
@@ -0,0 +1,24 @@
+let marks = 30;
+
+if (marks >= 80 ) {
+  console.log("A+");
+} else if ( marks >= 70 ) {
+  console.log("A");
+  
+} else if ( marks >= 60 ) {
+  console.log("A-");
+  
+} else if (marks >= 50) {
+  console.log("B+");
+  
+} else if ( marks >= 40 ){
+  console.log("B");
+  
+}else if ( marks >= 30) {
+  console.log("c");
+  
+}
+else {
+  console.log("Fail");
+  
+}
