271d78f1c37c8835301e753146010c39016f0645
toufik0661 <toufiqurrahman0661@gmail.com>
2025-10-07 22:49:57 +0600

    Login check in if-else

---
+++ Login-Check.js
@@ -0,0 +1,10 @@
+let username = "Toufik";
+let password = "12345";
+
+if ( username === "Toufik" && password === "12345") {
+  console.log("Login Successful!");
+
+} else {
+  console.log("Invalid username or password!");
+  
+}
